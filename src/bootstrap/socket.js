36b9ddc18f278025d70cfed47c01b363226ad2c9
khanh2906 <khanhphungdai@gmail.com>
2024-04-20 22:56:06 +0700

    Replace from kernel socket to babmimi-socket.io

--- src/bootstrap/socket.js
+++ src/bootstrap/socket.js
@@ -4,7 +4,9 @@ const { createServer } = require('node:http');
 const config = require("./../configs/socket");
 const app = express();
 const server = createServer(app);
-require("./../kernel/socket/handle")(server, config)
+const socket = require("@knfs-tech/bamimi-socket.io")
+socket.io(server, config)
+require("./../routes/socket/abc")
 
 server.listen(config.server.port, () => {
 	console.log(`server running at http://localhost:${config.server.port}`);
